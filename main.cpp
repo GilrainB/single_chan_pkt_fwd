2d4d0eb859ac59c47ab0bc55a02bb68d545accf4
GilrainB <gilrain25@gmail.com>
2017-09-12 18:41:15 +0200

    Old configuration, Younes' account.

--- main.cpp
+++ main.cpp
@@ -74,18 +74,18 @@ sf_t sf = SF7;
 uint32_t  freq = 868100000; // in Mhz! (868.1)
 
 // Set location
-float lat=0.0;
-float lon=0.0;
-int   alt=0;
+float lat=51.916622;
+float lon=4.46335460;
+int   alt=15;
 
 /* Informal status fields */
 static char platform[24]    = "Single Channel Gateway";  /* platform definition */
-static char email[40]       = "";                        /* used for contact email */
-static char description[64] = "";                        /* used for free form description */
+static char email[40]       = "y.elbali88@gmail.com";                        /* used for contact email */
+static char description[64] = "developer";                        /* used for free form description */
 
 // define servers
 // TODO: use host names and dns
-#define SERVER1 "54.72.145.119"    // The Things Network: croft.thethings.girovito.nl
+#define SERVER1 "52.169.76.203"    // The Things Network: croft.thethings.girovito.nl
 //#define SERVER2 "192.168.1.10"      // local
 #define PORT 1700                   // The port on which to send data
 
